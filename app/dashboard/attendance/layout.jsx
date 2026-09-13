d0a27f2e3111cd727f8980d734c875cb90432f06
MD Muntasir Alam Resti <muntasiralamresti@gmail.com>
2026-09-14 02:06:33 +0600

    Add Attendance Grid/List layout switcher

---
+++ app/dashboard/attendance/layout.jsx
@@ -0,0 +1,5 @@
+import ViewModeLayout from "@/components/ViewModeLayout";
+
+export default function AttendanceLayout({ children }) {
+  return <ViewModeLayout page="attendance">{children}</ViewModeLayout>;
+}
